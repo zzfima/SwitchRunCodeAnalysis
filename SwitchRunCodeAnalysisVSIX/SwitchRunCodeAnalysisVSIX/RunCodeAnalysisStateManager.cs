d7703e34136c00b8f12d196283ed397af2e1a621
zzfima <zzfima@gmail.com>
2022-10-05 10:51:15 +0300

    impl on off for specific sln

---
+++ SwitchRunCodeAnalysisVSIX/SwitchRunCodeAnalysisVSIX/RunCodeAnalysisStateManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+
+namespace SwitchRunCodeAnalysisVSIX
+{
+    internal class RunCodeAnalysisStateManager
+    {
+        private readonly string _solutionFilePath;
+
+        public RunCodeAnalysisStateManager(string solutionFilePath)
+        {
+            _solutionFilePath = solutionFilePath;
+        }
+
+        public void TurnOn()
+        {
+            var solutionFile = SolutionFile.Parse(_solutionFilePath);
+
+            foreach (var csproj in solutionFile.ProjectsInOrder)
+            {
+                var projectCollection = new ProjectCollection();
+                var loadedProject = projectCollection.LoadProject(csproj.AbsolutePath);
+                loadedProject.SetProperty("RunCodeAnalysis", "true");
+                loadedProject.Save();
+            }
+        }
+
+        public void TurnOff()
+        {
+            var solutionFile = SolutionFile.Parse(_solutionFilePath);
+
+            foreach (var csproj in solutionFile.ProjectsInOrder)
+            {
+                var projectCollection = new ProjectCollection();
+                var loadedProject = projectCollection.LoadProject(csproj.AbsolutePath);
+                loadedProject.SetProperty("RunCodeAnalysis", "false");
+                loadedProject.Save();
+            }
+        }
+    }
+}
